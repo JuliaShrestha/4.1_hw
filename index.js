a4b1a92723b9804e86596f3da5d97a3251346277
JuliaShrestha <juliashrestha215@gmail.com>
2024-07-16 17:53:22 +0300

    Prompt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const userName = prompt('What is your name?');
 
-if (!userName) {
+if (userName === null) {
+    alert("Please, enter your name!");
+} else if (!userName.trim() || !isNaN(userName)) {
     alert("The name is not entered!");
 } else {
     alert(`Hello, ${userName}! How are you?`);
